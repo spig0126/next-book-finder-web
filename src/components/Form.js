f4fa97ef19848b9c448320020d7b53e261eef1ad
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-13 16:15:30 +0900

    Create Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,45 @@
-export default function Form(){
-    return(
-        <div>
-            Form
-        </div>
-    )
+import { useState } from "react";
+import styled from "styled-components";
+
+export default function Form({ APIData, setAPIData }) {
+  const [searchKeyword, setsearchKeyword] = useState("");
+
+  const handleChange = (e) => {
+    setsearchKeyword(e.target.value);
+    console.log(searchKeyword);
+  };
+
+  const submitData = (e) => {
+    e.preventDefault();
+    callRequest();
+  };
+  const callRequest = () => {
+   
+  };
+  const enterKeyPress = () => {
+    if (window.event.keyCode === 13) {
+      callRequest();
+    }
+  };
+
+  return (
+    <SearchForm onSubmit={submitData}>
+      <select name="searchRange" required>
+        <option value="all">전체</option>
+        <option value="title">제목</option>
+        <option value="author">저자</option>
+        <option value="publisher">출판사</option>
+      </select>
+      <Input
+        placeholder="작가 또는 제목을 입력하세요"
+        value={searchKeyword}
+        onChange={handleChange}
+        onKeyPress={enterKeyPress}
+      ></Input>
+      <Button>Search</Button>
+    </SearchForm>
+  );
 }
+const SearchForm = styled.form``;
+const Input = styled.input``;
+const Button = styled.button``;
