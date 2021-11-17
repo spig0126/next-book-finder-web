4a4c1d0df38219a8cbb756cca686b3c61a6ecb72
spig0126 <spig0126@gmail.com>
2021-11-17 13:03:00 +0900

    Add : Create useAxiosGet.js custom hook

--- src/library/useAxiosGet.js
+++ src/library/useAxiosGet.js
@@ -1,44 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function useAxiosGet(selectValue) {
-    const [totalItems, setTotalItems] = useState(0);
+function useAxiosGet(selectValue, inputData) {
+    const [totalItems, setTotalItems] = useState(null);
     const [bookArray, setBookArray] = useState([]);
 
-    const searchField = (searchRange) => {    //change API request link fields by 'searchRange'
+    function searchField(inputData, searchRange) {
+        //change API request link fields by 'searchRange'
         switch (searchRange) {
-            case 'all':
-                return `q=${inputData.replace(' ', '+')}`;
-            case 'title':
+            case "all":
+                return `q=${inputData.replace(" ", "+")}`;
+            case "title":
                 return `q=''+intitle:${inputData}`;
-            case 'author':
+            case "author":
                 return `q=''+inauthor:${inputData}`;
-            case 'publisher':
+            case "publisher":
                 return `q=''+inpublisher:${inputData}`;
         }
     }
 
-    useEffect(() => {
-        axios
-            .get(
-                `https://www.googleapis.com/books/v1/volumes?${searchField(selectValue)}`
-            )
-            //add fields request parameter(가져올 데이터 제한하기)
-            .then(function (response) {
-                setTotalItems(response.data.totalItems);
-                setBookArray(response.data.items);
+    async function refetch(inputData, searchRange) {
+        useEffect(() => {
+            axios
+                .get(
+                    `https://www.googleapis.com/books/v1/volumes?${searchField(
+                        inputData,
+                        searchRange
+                    )}`
+                )
+                //add fields request parameter(가져올 데이터 제한하기)
+                .then(function (response) {
+                    setTotalItems(response.data.totalItems);
+                    setBookArray(response.data.items);
 
-                if (response.data.items.length === 0) {
-                    // return (
-                    //     <ToastMessage>
-                    //         검색된 결과가 없습니다.<br />다시 검색해주세요.
-                    //     </ToastMessage>
-                    // );
-                } else {
-                    setAPIData(response.data);
-                }
-            });
-        console.log(APIData);
-    }, []);
+                    // if (response.data.items.length === 0) {
+                    //     // return (
+                    //     //     <ToastMessage>
+                    //     //         검색된 결과가 없습니다.<br />다시 검색해주세요.
+                    //     //     </ToastMessage>
+                    //     // );
+                    // } else {
 
-    return {totalItems, bookArray};
+                    // }
+                });
+            console.log({ totalItems, bookArray });
+            console.log(searchField(inputData, searchRange));
+        }, []);
+    }
+
+    return [{ totalItems, bookArray }, refetch];
 }
+
+export default useAxiosGet;
