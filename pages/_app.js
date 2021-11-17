176d611940b038d30f8534320b6ac0ecceba77ef
spig0126 <spig0126@gmail.com>
2021-11-17 13:23:20 +0900

    Style: add missing semi colon

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import * as React from "react"
+import * as React from "react";
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
 
-function App({ Component }) {
-  // 2. Use at the root of your app
-  return (
-    <ChakraProvider>
-      <Component />
-    </ChakraProvider>
-  )
+export default function App({ Component }) {
+    // 2. Use at the root of your app
+    return (
+        <ChakraProvider>
+            <Component />
+        </ChakraProvider>
+    );
 }
