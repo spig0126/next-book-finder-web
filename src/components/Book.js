1cdb9cea43f04f3f9f04cef36526c8e4360cf0d5
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-26 21:54:19 +0900

    Feat: Reuse book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,52 +1,22 @@
 import styled from "styled-components";
 import { Button, Wrap } from "@chakra-ui/react";
 import Link from "next/link";
-export default function Book() {
-    const bookList = [
-        {
-            title: "어린왕자",
-            authors: "작가",
-            publisher: "출판사",
-            published: "출판 날짜",
-            totalCount: "검색된 책 수",
-            id: "id",
-            etag: "etag",
-            pageCount: "쪽수",
-            averageRating: "평점",
-            ratingsCount: "평가인원",
-            description: "상세설명",
-            butLink: "구매링크",
-        },
-        {
-            title: "아기돼지삼형제",
-            authors: "작가",
-            publisher: "출판사",
-            published: "출판 날짜",
-            totalCount: "검색된 책 수",
-            id: "id",
-            etag: "etag",
-            pageCount: "쪽수",
-            averageRating: "평점",
-            ratingsCount: "평가인원",
-            description: "상세설명",
-            butLink: "구매링크",
-        },
-    ];
+
+export default function Book({ book }) {
     return (
-        <Link href={`/bookDescriptions/${bookList[0].title}`} passHref>
+        <Link href={`/bookDescriptions/${book.title}`} passHref>
             <a>
                 <Button But width="15rem" height="10rem">
                     <Wrap spacing="0.3rem" align="center">
                         <BookImage src="add image link" />
-                        <Title>{bookList[0].title}</Title>
-                        <Authors>{bookList[0].authors}</Authors>
-                        <Page>{bookList[0].pageCount}</Page>
+                        <Title>{book.title}</Title>
+                        <Authors>{book.authors}</Authors>
+                        <Page>{book.pageCount}</Page>
                         <Publish>
-                            {bookList[0].publisher} {bookList[0].published}
+                            {book.publisher} {book.published}
                         </Publish>
                         <Rating>
-                            {bookList[0].averageRating}{" "}
-                            {bookList[0].ratingsCount}
+                            {book.averageRating} {book.ratingsCount}
                         </Rating>
                     </Wrap>
                 </Button>
--- src/components/BookFindList.js
+++ src/components/BookFindList.js
@@ -1,9 +1,40 @@
 import Book from "./Book";
 export default function BookFindList() {
     // 2. Use at the root of your app
+    const bookList = [
+        {
+            title: "어린왕자",
+            authors: "작가",
+            publisher: "출판사",
+            published: "출판 날짜",
+            totalCount: "검색된 책 수",
+            id: "id",
+            etag: "etag",
+            pageCount: "쪽수",
+            averageRating: "평점",
+            ratingsCount: "평가인원",
+            description: "상세설명",
+            butLink: "구매링크",
+        },
+        {
+            title: "아기돼지삼형제",
+            authors: "작가",
+            publisher: "출판사",
+            published: "출판 날짜",
+            totalCount: "검색된 책 수",
+            id: "id",
+            etag: "etag",
+            pageCount: "쪽수",
+            averageRating: "평점",
+            ratingsCount: "평가인원",
+            description: "상세설명",
+            butLink: "구매링크",
+        },
+    ];
     return (
         <div>
-            <Book />
+            <Book book={bookList[0]} />
+            <Book book={bookList[1]} />
         </div>
     );
 }
