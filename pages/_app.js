954ef3c3cc827d2b9f89a01dce09aba6f74e91e5
spig0126 <spig0126@gmail.com>
2021-11-27 15:32:16 +0900

    Style: delete unnecessary comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import * as React from "react";
 
-// 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component }) {
-    // 2. Use at the root of your app
     return (
         <ChakraProvider>
             <Component />
