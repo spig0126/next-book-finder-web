8867db89bc0fdd8c465a08951a9d5c5b6f22b3a7
spig0126 <spig0126@gmail.com>
2021-11-27 13:49:26 +0900

    🚧 Solving API request

--- src/library/useAxiosGet.js
+++ src/library/useAxiosGet.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function useAxiosGet(selectValue, inputData) {
-    const [totalItems, setTotalItems] = useState(null);
+function useAxiosGet(inputData, searchRange) {
+    const [totalItems, setTotalItems] = useState(0);
     const [bookArray, setBookArray] = useState([]);
 
-    function searchField(inputData, searchRange) {
+    function searchField(inputData, selectValue) {
         //change API request link fields by 'searchRange'
-        switch (searchRange) {
+        switch (selectValue) {
             case "all":
                 return `q=${inputData.replace(" ", "+")}`;
             case "title":
@@ -19,36 +19,23 @@ function useAxiosGet(selectValue, inputData) {
         }
     }
 
-    async function refetch(inputData, searchRange) {
-        useEffect(() => {
-            axios
-                .get(
-                    `https://www.googleapis.com/books/v1/volumes?${searchField(
-                        inputData,
-                        searchRange
-                    )}`
-                )
-                //add fields request parameter(가져올 데이터 제한하기)
-                .then(function (response) {
-                    setTotalItems(response.data.totalItems);
-                    setBookArray(response.data.items);
+    
+    useEffect(() => {
+        const response = axios.get(`https://www.googleapis.com/books/v1/volumes?${searchField(
+            inputData,
+            searchRange
+        )}`);
+        setTotalItems(response.totalItems);
+        setBookArray(response.items);
+            
+    }, []);
 
-                    // if (response.data.items.length === 0) {
-                    //     // return (
-                    //     //     <ToastMessage>
-                    //     //         검색된 결과가 없습니다.<br />다시 검색해주세요.
-                    //     //     </ToastMessage>
-                    //     // );
-                    // } else {
+    // useEffect(() => {
+        console.log({ totalItems, bookArray });
+        console.log(searchField(inputData, searchRange));
+    // }, []);
 
-                    // }
-                });
-            console.log({ totalItems, bookArray });
-            console.log(searchField(inputData, searchRange));
-        }, []);
-    }
-
-    return [{ totalItems, bookArray }, refetch];
+    return { totalItems, bookArray };
 }
 
 export default useAxiosGet;
