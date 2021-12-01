1afda0c2e2b722f3d1355f1e38080624a9749f85
spig0126 <spig0126@gmail.com>
2021-12-01 11:27:51 +0900

    Feat: Add image

--- src/components/bestSellers/BestSellerBookCard.js
+++ src/components/bestSellers/BestSellerBookCard.js
@@ -1,9 +1,9 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Image } from "@chakra-ui/react"
 
-export default function BestSellerBookCard() {
+export default function BestSellerBookCard({book, main}) {
     return (
-        <Flex>
-
+        <Flex h={() => !main && "80%"} alignItems="center" >
+            <Image src={book.book_image} h={() => !main && "80%"} borderRadius="1rem" mx="1rem"/>
         </Flex>
     )
 }
