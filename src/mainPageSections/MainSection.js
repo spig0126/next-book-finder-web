de32606e3a260f6f6ec798cb61b6438a86973eff
spig0126 <spig0126@gmail.com>
2021-12-04 05:17:47 +0900

    Feat: merge MainSection and SearchSection

--- src/mainPageSections/MainSection.js
+++ src/mainPageSections/MainSection.js
@@ -1,6 +1,6 @@
-import { Flex, Heading, Image } from "@chakra-ui/react"
+import { Flex, Heading, Spacer } from "@chakra-ui/react"
 
-import MainBackground from "../../public/images/MainBackground2.jpg";
+import SearchForm from "../components/SearchForm";
 
 export default function MainSection() {
     return (
@@ -15,14 +15,22 @@ export default function MainSection() {
             >
                 SEARCH, <br />DISCOVER, <br />EXPLORE
             </Heading>
-            <Image
-                src={MainBackground.src}
-                h="25rem"
-                minH="inherit"
-                w="100%"
-                objectFit="cover"
-                mt="5rem"
-            />
+            <Flex
+                w="90%"
+                h="20rem"
+                m="7rem 0 10rem 0"
+                pl="10rem"
+                flexDir="column"
+                alignItems="right"
+                border="solid 1rem"
+                borderColor="yellow"
+                borderRightWidth="0"
+            >
+                <Spacer />
+                <Heading fontSize="6xl" color="blue">Ready for another book?</Heading>
+                <SearchForm width="40%" margin="1rem 0 0" />
+                <Spacer />
+            </Flex>
         </Flex>
     );
 }
