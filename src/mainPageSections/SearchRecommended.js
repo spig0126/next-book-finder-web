b67240dd2c2187bfc7315412e6b39fc18c60118d
spig0126 <spig0126@gmail.com>
2021-12-04 03:47:23 +0900

    Design: change color and margin

--- src/mainPageSections/SearchRecommended.js
+++ src/mainPageSections/SearchRecommended.js
@@ -6,8 +6,8 @@ import BestSellersList from "../components/bestSellers/BestSellersList";
 export default function SearchRecommended() {
     return (
         <Flex w="100%" flexDir="column" alignItems="center" mt="10rem">
-            <Heading fontSize="5xl">Ready for another book?</Heading>
-            <SearchForm width="50%" margin="1.5rem auto" />
+            <Heading fontSize="5xl" color="blue">Ready for another book?</Heading>
+            <SearchForm width="50%" margin="1rem auto" />
             <BestSellersList genre="all" button={false}/>
         </Flex>
     )
