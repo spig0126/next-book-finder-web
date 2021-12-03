2a70f2ae8389d113f5cb659a1f23a49a82ce9ad3
spig0126 <spig0126@gmail.com>
2021-12-04 03:49:01 +0900

    Style: Remove dead code

--- src/components/bestSellers/BestSellerBookCard.js
+++ src/components/bestSellers/BestSellerBookCard.js
@@ -1,8 +1,6 @@
 import { Flex, Image, Text, Box } from "@chakra-ui/react";
-import { useDisclosure } from "@chakra-ui/hooks";
 
 export default function BestSellerBookCard({ book }) {
-    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
         <Flex flexDir="column" w="100%" h="100%" >
             <Image
