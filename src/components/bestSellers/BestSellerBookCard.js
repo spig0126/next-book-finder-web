c1f28b451e47e755943cdbfc1cce8e86896c3eb3
spig0126 <spig0126@gmail.com>
2021-12-04 01:49:44 +0900

    Style: Change css and add Texts

--- src/components/bestSellers/BestSellerBookCard.js
+++ src/components/bestSellers/BestSellerBookCard.js
@@ -1,9 +1,21 @@
-import { Flex, Image } from "@chakra-ui/react"
+import { Flex, Image, Text, Box } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/hooks";
 
-export default function BestSellerBookCard({book, main}) {
+export default function BestSellerBookCard({ book }) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <Flex h={() => !main && "80%"} alignItems="center" >
-            <Image src={book.book_image} h={() => !main && "80%"} borderRadius="1rem" mx="1rem"/>
+        <Flex flexDir="column" w="100%" h="100%" >
+            <Image
+                src={book.book_image}
+                objectFit="fill"
+                w="100%"
+                maxW="100%"
+                h="80%"
+                minH="80%"
+                borderRadius="0.5rem"
+            />
+            <Text fontWeight="bold" color="white" mt="1rem">{book.title}</Text>
+            <Text color="white">{book.author}</Text>
         </Flex>
-    )
+    );
 }
