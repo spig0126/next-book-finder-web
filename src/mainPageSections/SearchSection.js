585347144a27b58cca159e6242ce2e2c85467c85
spig0126 <spig0126@gmail.com>
2021-12-04 05:02:10 +0900

    Rename: Rename as SearchSection from SearchRecommended

--- src/mainPageSections/SearchSection.js
+++ src/mainPageSections/SearchSection.js
@@ -8,7 +8,6 @@ export default function SearchRecommended() {
         <Flex w="100%" flexDir="column" alignItems="center" mt="10rem">
             <Heading fontSize="5xl" color="blue">Ready for another book?</Heading>
             <SearchForm width="50%" margin="1rem auto" />
-            <BestSellersList genre="all" button={false}/>
         </Flex>
     )
 }
