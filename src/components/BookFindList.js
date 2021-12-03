043832dc232b18e9c0747649924c0295c3012c2a
spig0126 <spig0126@gmail.com>
2021-12-04 03:50:20 +0900

    Design: Change Wrap to Grid for better layout

--- src/components/BookFindList.js
+++ src/components/BookFindList.js
@@ -1,30 +1,30 @@
-import Book from "./BookCard";
+import BookCard from "./BookCard";
 
-import { Wrap, Text, Flex } from "@chakra-ui/layout";
+import { Grid, Text, Flex, useBreakpointValue } from "@chakra-ui/react";
 import { useResultContext } from "../context/context";
 export default function BookFindList() {
     const { bookData } = useResultContext();
+    const breakpoint = useBreakpointValue({ base: 2, lg:3, xl:4 });
 
     const books =
         bookData.items &&
         bookData.items.map((book, index) => (
-            <Book key={book.id} index={index} book={book} />
+            <BookCard key={book.id} index={index} book={book} />
         ));
     
     return (
         <Flex
             flexDir="column"
             py={{ base: "1rem", md: "3rem" }}
-            px={{ base: "5%", md: "5rem" }}
+            px={{ base: "5%", md: "10rem" }}
             alignItems="center"
             color="blue"
         >
-            <Text ontSize="2xl" mt="2rem" h="1.5rem">Your</Text>
-            <Text fontSize="4xl" fontWeight="bold" mb="2rem"> SEARCH RESULTS ({bookData.totalItems})</Text>
-
-            <Wrap name="BookContainer" direction="row" spacing={5}>
+            <Text ontSize="3xl" mt="2rem" h="1.5rem">Your</Text>
+            <Text fontSize="5xl" fontWeight="bold" mb="2rem" fontWeight="bold"> SEARCH RESULTS ({bookData.totalItems})</Text>
+            <Grid templateColumns={`repeat(${breakpoint}, 1fr)`} gap="3rem">
                 {books}
-            </Wrap>
+            </Grid>
         </Flex>
     );
 }
