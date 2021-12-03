ee8ffd80eb432af6d84c1fe1299a19772ce3069d
spig0126 <spig0126@gmail.com>
2021-12-04 04:35:55 +0900

    Design: Apply css to [bookDescription] page

--- pages/bookDescriptions/[bookDescription].js
+++ pages/bookDescriptions/[bookDescription].js
@@ -1,27 +1,40 @@
-import styled from "styled-components";
-import { Wrap } from "@chakra-ui/react";
+import { Flex, Image, Grid, Button, Heading, Box } from "@chakra-ui/react";
 
 import { useRouter } from "next/router";
-import { Button } from "@chakra-ui/button";
+
 export default function bookDescription() {
     const router = useRouter();
     const { title, thumbnail, buyLink, description } = router.query;
 
     return (
-        <Wrap spacing="0.3rem" align="center">
-            <BookImage src={thumbnail} />
-            <Description>{description}</Description>
-
-            <Button
-                onClick={() => {
-                    router.push(buyLink);
-                }}
-            >
-                Buy
-            </Button>
-        </Wrap>
+        <Grid templateColumns="2fr 3fr" w="100vw" h="100vh" justifyItems="flex-end" alignItems="center">
+            <Flex bgColor="beige" w="50%" minW="50%" h="40%" minH="40%" alignItems="center" mr="10%">
+                <Image
+                    src={thumbnail}
+                    h="80%"
+                    mihH="80%"
+                    w="50"
+                    minW="50%"
+                    mx="auto"
+                    objectFit="cover"
+                    boxShadow="base"
+                />
+                
+            </Flex>
+            <Flex justifySelf="flex-start" flexDir="column" w="80%">
+                <Heading textAlign="left" fontSize="7xl" color="blue" >{title}</Heading>
+                <Box w="40%" h="2px" bgColor="yellow" my="5%" />
+                <Flex h="30vh" overflowY="scroll">{description}</Flex>
+                <Button
+                    mt="3rem"
+                    w="5rem"
+                    onClick={() => {
+                        router.push(buyLink);
+                    }}
+                >
+                    Buy
+                </Button>
+            </Flex>  
+        </Grid>
     );
 }
-
-const Description = styled.div``;
-const BookImage = styled.img``;
