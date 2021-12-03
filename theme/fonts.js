47ea1fd6c4a9b24b8ba0a4741bf9583cf0891ed4
spig0126 <spig0126@gmail.com>
2021-12-04 06:35:08 +0900

    Design: Add font

--- theme/fonts.js
+++ theme/fonts.js
@@ -1,7 +1,8 @@
 import { Global } from "@emotion/react";
 
 const Fonts = () => (
-    <Global styles={`
+    <Global
+        styles={`
         /* cyrillic */
         @font-face {
         font-family: 'Playfair Display';
@@ -56,15 +57,26 @@ const Fonts = () => (
         src: url(https://fonts.gstatic.com/s/playfairdisplay/v25/nuFvD-vYSZviVYUb_rj3ij__anPXJzDwcbmjWBN2PKd3vXDXbtXK-F2qC0s.woff) format('woff');
         unicode-range:U+0020-002F, U+003A-0040, U+005B-0060, U+007B-007E, U+0061-007A, U+0041-005A;
         }
+        /* latin-ext */
         @font-face {
-        font-family: 'Gothic A1';
-        font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-        src: url(https://fonts.gstatic.com/s/gothica1/v8/CSR94z5ZnPydRjlCCwl6ac0oQNM8ru4reAXaTlFCBXrC-se62sS-fg.0.woff2) format('woff2');
-        unicode-range: U+AC00-U+D7A3, U+0030-0039, U+0020-002F, U+003A-0040, U+005B-0060, U+007B-007E
+            font-family: 'Raleway';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: url(https://fonts.gstatic.com/s/raleway/v18/1Ptxg8zYS_SKggPN4iEgvnHyvveLxVvaorCGPrcVIT9d0c-dYA.woff) format('woff');
+            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
         }
-    `} />
+        /* latin */
+        @font-face {
+            font-family: 'Raleway';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: url(https://fonts.gstatic.com/s/raleway/v18/1Ptxg8zYS_SKggPN4iEgvnHyvveLxVvaorCIPrcVIT9d0c8.woff) format('woff');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+    `}
+    />
 );
 
 export default Fonts;
