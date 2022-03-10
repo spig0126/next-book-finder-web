82277ae3d2fb9e5f48a54c7c8b12233d441ebf55
spig0126 <spig0126@gmail.com>
2022-03-11 00:47:49 +0900

    Refactor: create BestSellers for better structure

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,59 @@
 import { Flex } from "@chakra-ui/layout";
+import axios from "axios";
 
-import BestSellersList from "../src/components/bestSellers/BestSellersList";
+import BestSellers from "../src/components/bestSellers/BestSellers";
 import MainSection from "../src/mainPageSections/MainSection";
 
-export default function Home() {
+function Home() {
     return (
         <Flex m="0" p="0" flexDir="column">
+
             <MainSection />
-            <Flex
-                mx="auto"
-                my="20rem"
-                w={{ base: "100vw", lg: "90vw" }}
-                h="90rem"
-                bgColor="blue"
-                zIndex="-1"
-                position="absolute"
-                top="13rem"
-                left={{ base: "0", lg: "5vw" }}
-            ></Flex>
-            <Flex
-                mx="auto"
-                my="20rem"
-                w={{ base: "100vw", lg: "90vw" }}
-                h="50rem"
-                bgColor="yellow"
-                zIndex="-1"
-                position="absolute"
-                top="150rem"
-                left={{ base: "0", lg: "5vw" }}
-            ></Flex>
-            <Flex flexDir="column" w="100%" alignItems="center">
-                <BestSellersList
-                    genre="Fiction"
-                    hlColor="white"
-                    query="hardcover-fiction"
-                />
-                <BestSellersList
-                    genre="Nonfiction"
-                    hlColor="white"
-                    query="hardcover-nonfiction"
-                />
-                <BestSellersList
-                    genre="Young Adult's"
-                    hlColor="blue"
-                    query="young-adult-hardcover"
-                />
-                <BestSellersList
-                    genre="Advice, How-To & Miscellaneous"
-                    hlColor="blue"
-                    query="advice-how-to-miscellaneous"
-                />
-            </Flex>
+            <BestSellers />
         </Flex>
     );
 }
+
+// export async function getStaticProps() {
+//     const { data } = await axios.get(
+//         `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${process.env.NEXT_PUBLIC_NYT_API_KEY}`
+//     );
+
+//     // const res = await fetch(
+//     //     `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${process.env.NEXT_PUBLIC_NYT_API_KEY}`
+//     // );
+//     // const data = await res.json();
+//     return { props: { data: data || {} } };
+// }
+
+// Home.getInitialProps = async function () {
+//     const res = await fetch(
+//         "https://api.covid19api.com/total/dayone/country/kr"
+//     );
+//     const data = await res.json();
+
+//     return {
+//         data: data
+//     };
+// };
+
+// export async function getStaticProps() {
+//     const response = await axios.get(
+//         `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=tjMAjnSX1uGpTJGgrI5ceHScPxTpQ9WM`
+//     );
+//     const bookData = await response;
+//     const fetchData = async () => {
+//         const response = await axios.get(
+//             `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=tjMAjnSX1uGpTJGgrI5ceHScPxTpQ9WM`
+//         );
+//         console.log(response.data);
+//         bookData = response.data;
+//     };
+//     return {
+//         props: {
+//             bookData,
+//         },
+//     };
+// }
+
+export default Home;
---
+++ src/components/bestSellers/BestSellers.js
@@ -0,0 +1,63 @@
+import { Flex } from "@chakra-ui/react";
+import BestSellersList from "./BestSellersList";
+import useFetchBestSellerData from "../../library/useFetchBestSellerData";
+
+function BestSellers() {
+    const { bestSellerData, isLoading, isError } = useFetchBestSellerData();
+
+    return (
+        <Flex flexDir="column" w="100%" alignItems="center">
+            <Flex
+                flexDir="column"
+                w="100%"
+                alignItems="center"
+                bgColor="blue"
+                py="5rem"
+                mb="40rem"
+            >
+                <BestSellersList
+                    genre="Fiction"
+                    hlColor="white"
+                    hoverColor="yellow"
+                    data={bestSellerData.fictionBooks}
+                    isLoading={isLoading}
+                />
+                <BestSellersList
+                    genre="Nonfiction"
+                    hlColor="white"
+                    hoverColor="yellow"
+                    data={bestSellerData.nonfictionBooks}
+                    isLoading={isLoading}
+                />
+            </Flex>
+            <Flex
+                flexDir="column"
+                w="100%"
+                alignItems="center"
+                bgColor="yellow"
+                hoverColor="blue"
+                py="5rem"
+                mb="5rem"
+            >
+                <Flex flexDir="column" position="relative" top="-30rem">
+                    <BestSellersList
+                        genre="Young Adult's"
+                        hlColor="blue"
+                        hoverColor="white"
+                        data={bestSellerData.youngAdultBooks}
+                        isLoading={isLoading}
+                    />
+                    <BestSellersList
+                        genre="Advice, How-To & Miscellaneous"
+                        hlColor="blue"
+                        hoverColor="white"
+                        data={bestSellerData.adviceBooks}
+                        isLoading={isLoading}
+                    />
+                </Flex>
+            </Flex>
+        </Flex>
+    );
+}
+
+export default BestSellers;
